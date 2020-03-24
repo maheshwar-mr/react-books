8c9b2a6d3ed8fe416855fb14c2c2b55257e24090
Maheshwar Reddy <37240803+maheshwar807@users.noreply.github.com>
2020-03-24 10:42:01 +0530

    Update ViewBook.js

--- src/ViewBook.js
+++ src/ViewBook.js
@@ -17,7 +17,7 @@ class ViewBook extends Component{
   state={
     Books:[] ,
     search:"",
-    ipa: "http://18.218.2.249:3001/Books/",
+    ipa: "http://0.0.0.0:3001/Books/",
     addBookDetails:
     {
       title:'',
